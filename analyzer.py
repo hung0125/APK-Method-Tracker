4b1588a1fa63412f7568783596f47b500d023690
hung0125 <65654501+hung0125@users.noreply.github.com>
2022-01-11 12:54:31 +0800

    Add files via upload

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,11 @@
 #just the draft, algo is not optimized and I'm quite busy recently :(
 import os
 import glob
+from time import time
 from os.path import basename
+from pathlib import Path
 
-jadx = input("Jadx decompiled dir (full path): ")
+jadx = input("Jadx decompiled dir, the INJECTED version (full path): ")
 trace = input("traceTmp folder (full path): ")
 
 javaDoc = [os.path.join(dp, f) for dp, dn, filenames in os.walk(jadx) for f in filenames if os.path.splitext(f)[1] == '.java']
@@ -31,9 +33,17 @@ for T in traceTmp:
                 
                 snippets.append(snipTmp)
                 found = True
+                print(f"Found: {basename(T[:-2])}")
                 break
-            
-for S in snippets:
-    print("Method:")
-    for _S in S:
-        print(_S)
+
+ts = int(time())
+os.mkdir(f"TraceDetail_{ts}")
+os.mkdir(f"TraceDetail_{ts}/trace")
+
+html = "<html><body>"
+for i in range(len(snippets)):
+    open(f"TraceDetail_{ts}/trace/{basename(traceTmp[i])}", "wb").write("\n".join(snippets[i]).encode("utf-8"))
+    html += f"<a href='{f'trace/{basename(traceTmp[i])}'}'>{i+1} | {basename(traceTmp[i][:-2])}</a><br>"
+
+html += "</body></html>"
+open(f"TraceDetail_{ts}/results.html", "w").write(html)
