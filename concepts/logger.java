2e3cd66861f911fff6b17e83b52ea59625b36ebd
hung0125 <65654501+hung0125@users.noreply.github.com>
2022-01-15 01:30:23 +0800

    Update logger.java

--- concepts/logger.java
+++ concepts/logger.java
@@ -1,21 +1,21 @@
 private static void logger20220108(String s)
-{
-    String[] detail = s.split("#");
-    int pos = Integer.valueOf(detail[1]);
-    int cap = Integer.valueOf(detail[2]);
-    long utime = System.currentTimeMillis() / 1000L;
+	{
+		String[] detail = s.split("#");
+		int pos = Integer.valueOf(detail[1]);
+		int cap = Integer.valueOf(detail[2]);
+		long utime = System.currentTimeMillis() / 1000L;
 
-    if(doseq20220108 == null)
-        doseq20220108 = new long[cap];
+		if(doseq20220108 == null)
+			doseq20220108 = new long[cap];
 
-    if(doseq20220108[pos] == 0 || utime - doseq20220108[pos] >= 1)
-        doseq20220108[pos] = utime;
-    else
-        return;
+		if(doseq20220108[pos] == 0 || utime - doseq20220108[pos] >= 1)
+			doseq20220108[pos] = utime;
+		else
+			return;
 
-    String[] cmd = {"sh", "-c", String.format("mkdir /sdcard/traceTmp & touch '/sdcard/traceTmp/%d#%s._'", System.nanoTime(), detail[0])};
+		String[] cmd = {"sh", "-c", String.format("mkdir /sdcard/traceTmp & echo '%d' > '/sdcard/traceTmp/%s._'", System.nanoTime(), detail[0])};
 
-    try{
-       Runtime.getRuntime().exec(cmd);
-    }catch(Exception e){}
-}
+		try{
+			Runtime.getRuntime().exec(cmd);
+		}catch(Exception e){}
+	}
