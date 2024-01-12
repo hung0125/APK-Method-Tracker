3aafd6cfcd9c4537a5f1e90574206a88dcc8c7a2
hung0125 <65654501+hung0125@users.noreply.github.com>
2024-01-13 03:02:51 +0800

    Update test.php

--- v3/web/test.php
+++ v3/web/test.php
@@ -1,9 +1,13 @@
 <?php
 try {
+  $logMessage = "[" . date('Y-m-d H:i:s') . "]: The server is processing recording...\n";
+  file_put_contents("log.txt", $logMessage,  FILE_APPEND | LOCK_EX);
 	$data = file_get_contents("php://input"); 
 	file_put_contents("output.txt", $data);
+  $logMessage = "[" . date('Y-m-d H:i:s') . "]: The server saved the recording...\n";
+  file_put_contents("log.txt", $logMessage,  FILE_APPEND | LOCK_EX);
 } catch (Exception $e) {
-    file_put_contents("error.txt", $data,  FILE_APPEND | LOCK_EX);
+    file_put_contents("error.txt", "error when processing upload: " . $e->getMessage() . "\n",  FILE_APPEND | LOCK_EX);
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
 ?>
