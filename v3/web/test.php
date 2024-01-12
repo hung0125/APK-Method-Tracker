b298f743b0126c8ef18b31a8be270c02f6e53c26
hung0125 <65654501+hung0125@users.noreply.github.com>
2024-01-13 01:43:29 +0800

    Update test.php

--- v3/web/test.php
+++ v3/web/test.php
@@ -1,7 +1,7 @@
 <?php
 try {
 	$data = file_get_contents("php://input"); 
-	file_put_contents("output.txt", $data,  LOCK_EX);
+	file_put_contents("output.txt", $data);
 } catch (Exception $e) {
     file_put_contents("error.txt", $data,  FILE_APPEND | LOCK_EX);
     echo 'Caught exception: ',  $e->getMessage(), "\n";
