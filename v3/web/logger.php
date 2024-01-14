35d59fe6f3b460bf5a2d0a80cee6a5adbbdaaa42
hung0125 <65654501+hung0125@users.noreply.github.com>
2024-01-14 19:11:32 +0800

    Add files via upload

--- v3/web/logger.php
+++ v3/web/logger.php
@@ -1,13 +1,14 @@
 <html>
-<h2>Downloads <button href="#" onclick="clearAll()">Clear Data</button></h2>
+<h2>Downloads <button href="#" onclick="clearData('dump')">Clear Data</button></h2>
 <iframe width=80% height=200 style="margin-left: 8%; margin-bottom: 20px;" src='./download.php'></iframe>
-<h2>Logs</h2>
+<h2>Logs <button href="#" onclick="clearData('logs')">Clear Logs</button></h2>
 <?php
     date_default_timezone_set('Asia/Singapore'); // Set the desired timezone (UTC+8)
 
     $step = $_GET['step'] ?? '';
     $lines = $_GET['lines'] ?? '';
     $action = $_GET['action'] ?? '';
+    $clear = $_GET['clear'] ?? '';
 
     if ($step == 1) {
         $logMessage = "[" . date('Y-m-d H:i:s') . "]: The client started recording...\n";
@@ -23,47 +24,57 @@
 
     // Read log file and convert it into an array of log messages
     $logFile = file_get_contents('log.txt');
-    $logMessages = explode("\n", trim($logFile));
-    $logMessages = array_reverse($logMessages); // Reverse the order of log messages
+    if (!empty($logFile)) {
+        $logMessages = explode("\n", trim($logFile));
+        $logMessages = array_reverse($logMessages); // Reverse the order of log messages
+    
+        // Create the table header
+        $tableHeader = "<tr><th>Timestamp</th><th>Log Message</th></tr>";
+    
+        // Create the table rows with log messages
+        $tableRows = '';
+        foreach ($logMessages as $log) {
+            list($timestamp, $message) = explode(']:', $log, 2);
+            $tableRows .= "<tr><td style='border: 1px solid black;'>{$timestamp}]</td><td style='border: 1px solid black;'>{$message}</td></tr>";
+        }
+    
+        // Create the HTML table
+        $table = "<table style='border: 1px solid black;'>{$tableHeader}{$tableRows}</table>";
 
-    // Create the table header
-    $tableHeader = "<tr><th>Timestamp</th><th>Log Message</th></tr>";
-
-    // Create the table rows with log messages
-    $tableRows = '';
-    foreach ($logMessages as $log) {
-        list($timestamp, $message) = explode(']:', $log, 2);
-        $tableRows .= "<tr><td style='border: 1px solid black;'>{$timestamp}]</td><td style='border: 1px solid black;'>{$message}</td></tr>";
+        echo $table;
+    } else {
+        echo '<h3>No Record Found.</h3>';
     }
-
-    // Create the HTML table
-    $table = "<table style='border: 1px solid black;'>{$tableHeader}{$tableRows}</table>";
-
-    echo $table;
 
     // Clear all files
     if (!empty($action) && $action == 'clear') {
-        $folders = [glob('dump/*'), glob('merged/*')];
+        if ($clear == 'dump') {
+            $folders = [glob('dump/*'), glob('merged/*')];
 
-        // Loop through each file and delete it
-        foreach ($folders as $fd) {
-            foreach ($fd as $file) {
-                if (is_file($file)) {
-                    unlink($file);
+            // Loop through each file and delete it
+            foreach ($folders as $fd) {
+                foreach ($fd as $file) {
+                    if (is_file($file)) {
+                        unlink($file);
+                    }
                 }
             }
+        } elseif ($clear == 'logs') {
+            file_put_contents("log.txt", "");
         }
+        
     }
 ?>
 
 <script>
-    function clearAll() {
-        alert("Cleared request sent.");
+    function clearData(param) {
+        alert("Clear " + param + " request sent.");
         // Make an AJAX request to the PHP function
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'logger.php?action=clear', true);
+        xhr.open('GET', 'logger.php?action=clear&clear='+param, true);
         xhr.onload = function() {};
         xhr.send();
     }
+
 </script>
 </html>
