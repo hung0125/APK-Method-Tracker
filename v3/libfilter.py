1dc6faa81d03051ef35262d9e31c23884f128870
hung0125 <65654501+hung0125@users.noreply.github.com>
2025-05-12 20:19:48 +0800

    Update libfilter.py identify built in RESTful libraries

--- v3/libfilter.py
+++ v3/libfilter.py
@@ -25,40 +25,56 @@ def find_uitext_calls(smali):
 
     return cnt
 
-def precheck_isActivity(smali) -> bool:
-    cont = open(smali, 'rb').read().decode('utf-8').splitlines()
+def precheck_isActivity(smali_cont: str) -> bool:
+    cont = smali_cont.splitlines()
     for L in cont:
         if L.startswith('.method ') and L.endswith(' onCreate(Landroid/os/Bundle;)V'):
             return True
     
     return False
 
+def precheck_useInternalHttpLib(smali_cont: str) -> bool:
+    keywords = ['Ljava/net/']
+    for K in keywords:
+        if K in smali_cont:
+            return True
+    return False
 
 def get_smali_files(dir_path):
     sorted_path = []
     upaths = {}
     is_activity = {}
+    use_internal_http_lib = {}
     for root, dirs, files in walk(dir_path):
         base_path = root[len(dir_path)+1:]
         for file in tqdm(files):
             if file.endswith(".smali"):
                 smali_path = os.path.join(root, file)
-                
-                if not base_path in is_activity and precheck_isActivity(smali_path):
-                    is_activity[base_path] = True
-                
+                smali_cont = None
+
+                if not base_path in is_activity:
+                    smali_cont = open(smali_path, 'rb').read().decode('utf-8')
+                    if precheck_isActivity(smali_cont):
+                        is_activity[base_path] = True 
+
                 if not base_path in upaths:
                     upaths[base_path] = True
                     sorted_path.append(base_path)
                 
+                if not base_path in use_internal_http_lib:
+                    if not smali_cont:
+                        smali_cont = open(smali_path, 'rb').read().decode('utf-8')
+                    if precheck_useInternalHttpLib(smali_cont):
+                        use_internal_http_lib[base_path] = True
                 
-    return [sorted_path, is_activity]
+    return [sorted_path, is_activity, use_internal_http_lib]
 
 base = input('Input decompiled base path: ')
 
 result = get_smali_files(base)
 roots = result[0]
 check_activity_class = result[1]
+check_use_internal_http_lib = result[2]
 
 if os.path.isfile('libkeep.txt'):
     os.remove('libkeep.txt')
@@ -78,10 +94,10 @@ for R in roots:
             break
     if in_basic_filter: continue
 
-    if R in check_activity_class:
-        outstr_important.append(str(f'!ACTIVITY_CLASS!->{R}' + '\n').encode('utf-8'))
+    if R in check_activity_class:       
+        outstr_important.append(str(f'!ACTIVITY_CLASS{"&BUILT_IN_REST" if R in check_use_internal_http_lib else ''}!->{R}' + '\n').encode('utf-8'))
     else:
-        outstr_general.append(str(f'!GENERAL!->{R}' + '\n').encode('utf-8'))
+        outstr_general.append(str(f'!GENERAL{"&BUILT_IN_REST" if R in check_use_internal_http_lib else ''}!->{R}' + '\n').encode('utf-8'))
 
 for L in outstr_important:
     open('libkeep.txt', 'ab').write(L)
